719801d200ac36958f66f25b3a9a84420cb5c2c0
huangjun <hj644473762@gmail.com>
2018-03-28 09:17:09 +0800

    update ios

--- PullToRefreshScrollView.ios.js
+++ PullToRefreshScrollView.ios.js
@@ -1,38 +1,82 @@
 'use strict';
+import React, {
+    Component,
+} from 'react';
 
-import React, {Component} from 'react';
+import {DeviceEventEmitter,NativeModules,findNodeHandle,ScrollView,processColor} from 'react-native'
+const {DWRefreshManager} = NativeModules
+const REF_PTR = "ptr_ref";
+const DROP_VIEW_DID_BEGIN_REFRESHING_EVENT = 'dropViewDidBeginRefreshing';
+let callbacks = {};
 
-import ReactNative, {UIManager,NativeModules,NativeAppEventEmitter,findNodeHandle,StyleSheet, processColor} from 'react-native';
 
-const { DWRefreshManager } = NativeModules;
-
-var DROP_VIEW_DID_BEGIN_REFRESHING_EVENT = 'dropViewDidBeginRefreshing';
-
-var callbacks = {};
-
-var subscription = NativeAppEventEmitter.addListener(
-  DROP_VIEW_DID_BEGIN_REFRESHING_EVENT,
-  (reactTag) => callbacks[reactTag]()
-);
 // subscription.remove();
 
-var PullToRefreshScrollView = {
-  configure: function(configs, callback) {
-    var nodeHandle = ReactNative.findNodeHandle(configs.node);
-    var options = {
-      tintColor: processColor(configs.tintColor),
-      activityIndicatorViewColor: processColor(configs.activityIndicatorViewColor)
-    };
-    DWRefreshManager.configure(nodeHandle, options, (error) => {
-      if (!error) {
-        callbacks[nodeHandle] = callback;
-      }
-    });
-  },
-  endRefreshing: function(node) {
-    var nodeHandle = ReactNative.findNodeHandle(node);
-    DWRefreshManager.endRefreshing(nodeHandle);
-  }
-};
+export default class PullToRefreshScrollView extends Component {
+    static defaultProps = {
+        durationToCloseHeader: 300,
+        durationToClose: 200,
+        resistance: 2,
+        ratioOfHeaderHeightToRefresh: 1.2,
+        refreshing:false,
+        refreshableTitlePull: '下拉刷新',
+        refreshableTitleRefreshing: '加载中...',
+        refreshableTitleRelease: '松手开始刷新',
+        refreshableTitleComplete: '刷新完成.',
+        dateTitle: '最后更新时间: ',
+        tintColor:"#05A5D1",
+        activityIndicatorViewColor: '#05A5D1'
+    }
+    constructor(props) {
+        super(props);
+        this._onRefresh = this._onRefresh.bind(this);
+    }
 
-module.exports = PullToRefreshScrollView;
+    componentDidMount() {
+      this.subscription = DeviceEventEmitter.addListener(
+            DROP_VIEW_DID_BEGIN_REFRESHING_EVENT,
+            (reactTag) => callbacks[reactTag]()
+        );
+
+        var options = {
+            tintColor: processColor(this.props.tintColor),
+            activityIndicatorViewColor: processColor(this.props.activityIndicatorViewColor)
+        };
+        let nodeHandle = findNodeHandle(this.refs[REF_PTR]);
+        DWRefreshManager.configure(nodeHandle, options, (error) => {
+            if (!error) {
+                callbacks[nodeHandle] = this._onRefresh
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.subscription && this.subscription.remove()
+    }
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.refreshing === false && nextProps.refreshing !== this.props.refreshing){
+            this.onRefreshEnd()
+        }
+    }
+    _onRefresh() {
+        if (!this.props.onRefresh) {
+            return;
+        }
+        this.props.onRefresh();
+    };
+
+    /**
+     * 刷新完成
+     */
+    onRefreshEnd() {
+        let nodeHandle = findNodeHandle(this.refs[REF_PTR]);
+        DWRefreshManager.endRefreshing(nodeHandle);
+    }
+    render() {
+        return (
+            <ScrollView ref={REF_PTR}>
+                {this.props.children}
+            </ScrollView>
+        );
+    }
+}
