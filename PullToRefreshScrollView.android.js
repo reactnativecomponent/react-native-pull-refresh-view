fc2b1e3a9158525ec92791cb5ca11d90d435e011
huangjun <hj644473762@gmail.com>
2018-04-03 09:52:52 +0800

    格式代码，添加注释

--- PullToRefreshScrollView.android.js
+++ PullToRefreshScrollView.android.js
@@ -1,19 +1,13 @@
 /**
  * Created by dowin on 2017/11/28.
  */
-'use strict';
-import React, {
-    Component,
-} from 'react';
-import {
-    requireNativeComponent,
-    ScrollView,
-    View,
-} from 'react-native';
-import PropTypes from 'prop-types';
-const UIManager = require('react-native/lib/UIManager');
-const ReactNative = require('react-native');
-const REF_PTR = "ptr_ref";
+'use strict'
+import React, {Component} from 'react'
+import {requireNativeComponent, ScrollView, View} from 'react-native'
+import PropTypes from 'prop-types'
+const UIManager = require('react-native/lib/UIManager')
+const ReactNative = require('react-native')
+const REF_PTR = "ptr_ref"
 
 export default class PullToRefreshScrollView extends Component {
 
@@ -25,54 +19,47 @@ export default class PullToRefreshScrollView extends Component {
         ratioOfHeaderHeightToRefresh: 1.2,
         pullToRefresh: false,
         keepHeaderWhenRefresh: true,
-        refreshing:false,
+        refreshing: false,
         refreshableTitlePull: '下拉刷新',
         refreshableTitleRefreshing: '加载中...',
         refreshableTitleRelease: '松手开始刷新',
         refreshableTitleComplete: '刷新完成.',
-        dateTitle: '最后更新时间: ',
+        dateTitle: '最后更新时间: '
     }
 
-    constructor(props) {
-        super(props);
-        this._onRefresh = this._onRefresh.bind(this);
+    constructor (props) {
+        super(props)
+        this._onRefresh = this._onRefresh.bind(this)
     }
-    componentWillReceiveProps(nextProps) {
-        if(nextProps.refreshing === false && nextProps.refreshing !== this.props.refreshing){
+    componentWillReceiveProps (nextProps) {
+        if (nextProps.refreshing === false && nextProps.refreshing !== this.props.refreshing) {
             this.onRefreshEnd()
         }
     }
-    _onRefresh() {
+    _onRefresh () {
         if (!this.props.onRefresh) {
-            return;
+            return
         }
-        this.props.onRefresh();
-    };
+        this.props.onRefresh()
+    }
 
-    /**
-     * 自动刷新
-     */
-    autoRefresh() {
-        let self = this;
+    autoRefresh () {
+        let self = this
         UIManager.dispatchViewManagerCommand(
             ReactNative.findNodeHandle(self.refs[REF_PTR]),
             1,
             null
-        );
+        )
     }
 
-    /**
-     * 刷新完成
-     */
-    onRefreshEnd() {
+    onRefreshEnd () {
         UIManager.dispatchViewManagerCommand(
             ReactNative.findNodeHandle(this.refs[REF_PTR]),
             0,
             null
-        );
+        )
     }
-
-    render() {
+    render () {
         // onPtrRefresh 事件对应原生的ptrRefresh事件
         return (
             <RCTPtrAndroid
@@ -84,11 +71,11 @@ export default class PullToRefreshScrollView extends Component {
                     {this.props.children}
                 </ScrollView>
             </RCTPtrAndroid>
-        );
+        )
     }
 }
 
-//PullToRefreshScrollView.name = "RCTPtrAndroid"; //便于调试时显示(可以设置为任意字符串)
+// PullToRefreshScrollView.name = "RCTPtrAndroid"; // 便于调试时显示(可以设置为任意字符串)
 PullToRefreshScrollView.propTypes = {
     onPtrRefresh: PropTypes.func,
     resistance: PropTypes.number,
@@ -103,7 +90,6 @@ PullToRefreshScrollView.propTypes = {
     refreshableTitleRelease: PropTypes.string,
     refreshableTitleComplete: PropTypes.string,
     dateTitle: PropTypes.string,
-    ...View.propTypes,
-};
-
-const RCTPtrAndroid = requireNativeComponent('RCTPtrAndroid', PullToRefreshScrollView, {nativeOnly: {onPtrRefresh: true}});
+    ...View.propTypes
+}
+const RCTPtrAndroid = requireNativeComponent('RCTPtrAndroid', PullToRefreshScrollView, {nativeOnly: {onPtrRefresh: true}})
--- PullToRefreshScrollView.ios.js
+++ PullToRefreshScrollView.ios.js
@@ -1,67 +1,56 @@
-'use strict';
-import React, {
-    Component,
-} from 'react';
-
-import {DeviceEventEmitter,NativeModules,findNodeHandle,ScrollView,processColor} from 'react-native'
+'use strict'
+import React, { Component} from 'react'
+import {DeviceEventEmitter, NativeModules, findNodeHandle, ScrollView, processColor} from 'react-native'
 const {DWRefreshManager} = NativeModules
-const REF_PTR = "ptr_ref";
-const DROP_VIEW_DID_BEGIN_REFRESHING_EVENT = 'dropViewDidBeginRefreshing';
-let callbacks = {};
-
-
-// subscription.remove();
+const REF_PTR = "ptr_ref"
+const DROP_VIEW_DID_BEGIN_REFRESHING_EVENT = 'dropViewDidBeginRefreshing'
+let callbacks = {}
 
 export default class PullToRefreshScrollView extends Component {
     static defaultProps = {
-        durationToCloseHeader: 500,//刷新完成延迟收起
-        // durationToClose: 200,
-        // resistance: 2,
-        // ratioOfHeaderHeightToRefresh: 1.2,
-        refreshing:false,
+        durationToCloseHeader: 500,// 刷新完成延迟收起
+        refreshing: false,
         refreshableTitlePull: '下拉刷新',
         refreshableTitleRefreshing: '加载中...',
         refreshableTitleRelease: '松手开始刷新',
         refreshableTitleComplete: '刷新完成',
         isShowLastTime: true,
-        tintColor:"#05A5D1",//用于type = 0
-        activityIndicatorViewColor: '#05A5D1'//用于type = 0
+        type: '1', // 刷新指示器的样式0、1
+        tintColor: "#05A5D1",// 限于type = 0
+        activityIndicatorViewColor: '#05A5D1'// 限于type = 0
     }
-    constructor(props) {
-        super(props);
-        this._onRefresh = this._onRefresh.bind(this);
+    constructor (props) {
+        super(props)
+        this._onRefresh = this._onRefresh.bind(this)
     }
-
-    componentDidMount() {
+    componentDidMount () {
       this.subscription = DeviceEventEmitter.addListener(
             DROP_VIEW_DID_BEGIN_REFRESHING_EVENT,
             (reactTag) => {
                 callbacks[reactTag]()
                 this.timer = setTimeout(() => {
-                    this.onRefreshEnd();
-                }, 3000);
-            });
-
-        var options = {
-            type:'0',
-            incremental:80,//刷新动画高度
-            strTitlePull:this.props.refreshableTitlePull,
-            strTitleRelease:this.props.refreshableTitleRelease,
-            strTitleRefreshing:this.props.refreshableTitleRefreshing,
-            strTitleComplete:this.props.refreshableTitleComplete,
-            durationToCloseHeader:this.props.durationToCloseHeader,
-            isShowLastTime:this.props.isShowLastTime,
+                    this.onRefreshEnd()
+                }, 3000)
+            })
+        let options = {
+            type: this.props.type,
+            incremental: 80,// 刷新动画高度
+            strTitlePull: this.props.refreshableTitlePull,
+            strTitleRelease: this.props.refreshableTitleRelease,
+            strTitleRefreshing: this.props.refreshableTitleRefreshing,
+            strTitleComplete: this.props.refreshableTitleComplete,
+            durationToCloseHeader: this.props.durationToCloseHeader,
+            isShowLastTime: this.props.isShowLastTime,
             tintColor: processColor(this.props.tintColor),
             activityIndicatorViewColor: processColor(this.props.activityIndicatorViewColor)
         };
-        let nodeHandle = findNodeHandle(this.refs[REF_PTR]);
+        let nodeHandle = findNodeHandle(this.refs[REF_PTR])
         DWRefreshManager.configure(nodeHandle, options, (error) => {
             if (!error) {
                 callbacks[nodeHandle] = this._onRefresh
             }
-        });
+        })
     }
-
     componentWillUnmount() {
         this.subscription && this.subscription.remove()
     }
@@ -72,19 +61,16 @@ export default class PullToRefreshScrollView extends Component {
     }
     _onRefresh() {
         if (!this.props.onRefresh) {
-            return;
+            return
         }
-        this.props.onRefresh();
-    };
-
-    /**
-     * 刷新完成
-     */
-    onRefreshEnd() {
-        let nodeHandle = findNodeHandle(this.refs[REF_PTR]);
-        DWRefreshManager.endRefreshing(nodeHandle);
+        this.props.onRefresh()
     }
-    render() {
+    // 刷新完成
+    onRefreshEnd () {
+        let nodeHandle = findNodeHandle(this.refs[REF_PTR])
+        DWRefreshManager.endRefreshing(nodeHandle)
+    }
+    render () {
         return (
             <ScrollView ref={REF_PTR}>
                 {this.props.children}
